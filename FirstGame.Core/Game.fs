a3c56a154140e20ab17414c51dfb01b6db2b1b4c
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-19 09:44:27 +0100

    Added first sample

--- FirstGame.Core/Game.fs
+++ FirstGame.Core/Game.fs
@@ -1,10 +1,7 @@
 ﻿namespace FirstGame.Core
 
-open System.Threading.Tasks
 open Urho
 open Urho.Gui
-open Urho.Physics
-open Urho.Actions
 
 module Game =
     
@@ -12,13 +9,18 @@ module Game =
         inherit Application(opt)
 
         override this.Start() =
+            base.Start()
+            
             let cache = this.ResourceCache
-
             let helloText = 
                 new Text(
                     Value = "Hello World from Urho3D, Mono, and F#",
                     HorizontalAlignment = HorizontalAlignment.Center,
                     VerticalAlignment = VerticalAlignment.Center)
-
+                    
             helloText.SetColor (new Color(0.f, 1.f, 0.f))
+            
+            let f = cache.GetFont("Fonts/Anonymous Pro.ttf")
+            helloText.SetFont(f, 30) |> ignore
+            
             this.UI.Root.AddChild(helloText)
